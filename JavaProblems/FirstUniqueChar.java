aaa658ee04dac5af43aa1d8aa04bc8d9e9d8655e
Pamula Indhu <146244249+Indhupamula@users.noreply.github.com>
2025-08-17 10:06:41 +0530

    Create FirstUniqueChar.java

---
+++ JavaProblems/FirstUniqueChar.java
@@ -0,0 +1,18 @@
+// JavaProblems/FirstUniqueChar.java
+import java.util.*;
+
+public class FirstUniqueChar {
+    public static int firstUniqChar(String s){
+        Map<Character,Integer> map = new HashMap<>();
+        for(char c: s.toCharArray()) map.put(c,map.getOrDefault(c,0)+1);
+        for(int i=0;i<s.length();i++){
+            if(map.get(s.charAt(i))==1) return i;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args){
+        String s="leetcode";
+        System.out.println("First unique index: "+firstUniqChar(s));
+    }
+}
