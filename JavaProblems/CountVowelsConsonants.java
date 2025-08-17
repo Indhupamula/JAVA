623ac79d4b370fc5cc8bdf936746d861aa7b80c5
Pamula Indhu <146244249+Indhupamula@users.noreply.github.com>
2025-08-17 10:02:59 +0530

    Create CountVowelsConsonants.java

---
+++ JavaProblems/CountVowelsConsonants.java
@@ -0,0 +1,14 @@
+// JavaProblems/CountVowelsConsonants.java
+public class CountVowelsConsonants {
+    public static void main(String[] args) {
+        String str = "Hello World";
+        int vowels=0, consonants=0;
+        for(char c: str.toLowerCase().toCharArray()) {
+            if(c>='a' && c<='z') {
+                if("aeiou".indexOf(c)>=0) vowels++;
+                else consonants++;
+            }
+        }
+        System.out.println("Vowels: "+vowels+", Consonants: "+consonants);
+    }
+}
